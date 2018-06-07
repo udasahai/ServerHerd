e3c518e568966e35c52927664b7a6c49165260ab
udasahai <udasahai@gmail.com>
2018-06-06 19:26:19 -0700

    added

--- server.py
+++ server.py
@@ -171,7 +171,7 @@ class EchoServerClientProtocol(asyncio.Protocol):
 			return
 
 
-		self.transport.write("? \n".encode()) 
+		self.transport.write("? {}".format(message).encode()) 
 
 
 	async def propagate(self, message):
